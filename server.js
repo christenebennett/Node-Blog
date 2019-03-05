09d89f3524ace631c7f8901649ee0349d38455a8
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-03-04 21:39:46 -0700

    Start set up of Users Router

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const logger = require('morgan');
+const helmet = require('helmet');
+
+const UsersRouter = require('./users/users-router');
 
 const server = express();
+const parser = express.json();
+const securityMiddleware = helmet();
+const logMiddleware = logger('dev');
+
+server.use(parser, securityMiddleware, logMiddleware)
+server.use('/api/users', UsersRouter);
 
 server.get('/', (req, res) => {
   res.send(`
---
+++ users/users-router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+const Users = require('../data/helpers/userDb');
+
+router.get('/')
+
+module.exports = router;

ebb5b4cb13253e76f0c889a276eacd78f9cf16c4
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-03-04 21:49:31 -0700

    Add GET request to Users Router

--- users/users-router.js
+++ users/users-router.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
-const Users = require('../data/helpers/userDb');
+const Users = require('../data/seeds/02-users');
 
-router.get('/')
+router.get('/', async (req, res) => {
+  try {
+    const users = await Users.get();
+    res.status(201).json(users);
+  } catch (error) {
+    res.status(500).json({err: "The users could not be retrieved."})
+  }
+})
 
 module.exports = router;

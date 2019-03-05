2a0a8b616ccf5394cc91155fd257d65b423ff9fa
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-03-05 14:14:29 -0700

    Add GET request for /api/users endpoint

--- users/users-router.js
+++ users/users-router.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-const Users = require('../data/seeds/02-users');
+const Users = require('../data/helpers/userDb');
+const Posts = require('../data/helpers/postDb')
 
 router.get('/', async (req, res) => {
   try {
-    const users = await Users.get();
+    const users = await Users.get(req.query);
     res.status(201).json(users);
   } catch (error) {
     res.status(500).json({err: "The users could not be retrieved."})
